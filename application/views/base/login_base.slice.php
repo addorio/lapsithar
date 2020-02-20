7e60f93cf3a2b40b74297c8c4ae137087aa03ee2
Mii <renaizumiume@gmail.com>
2020-02-20 10:35:03 +0700

    Remove Default Assets + favico
    - change Favico
    - remove default assets

--- application/views/base/login_base.slice.php
+++ application/views/base/login_base.slice.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>@php echo $title;@endphp</title>
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{APP_ASSETS}}images/favicon1.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{APP_ASSETS}}images/favicon.ico">
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
     <link href="{{APP_ASSETS}}css/style.css" rel="stylesheet">
 </head>
